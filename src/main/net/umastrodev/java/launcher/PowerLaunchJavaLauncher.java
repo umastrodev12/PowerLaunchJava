88e37ee7c66f0f09de66132a73a4856fb560625f
Aquele Astro Dev <astrochanneloficial@gmail.com>
2025-11-17 19:57:11 -0300

    A Configuração do Arquivo "PowerLaunchJava.java" está toda pronta para uso!

--- src/main/net/umastrodev/java/launcher/PowerLaunchJavaLauncher.java
+++ src/main/net/umastrodev/java/launcher/PowerLaunchJavaLauncher.java
@@ -1,3 +1,34 @@
 package net.umastrodev.powerlaunchjava.launcher
 
-...
+import net.dreamsbakery.launcher.powerlaunchjava;
+import net.umastrodev.powerlaunchjava.launch.utils;
+import net.perfect.tea.powerlaunchjava.workflow;
+import net.umastrodev.powerlaunchjava.launcher.files;
+
+public class PowerLauncherJavaLauncher {
+    public static void(String[] args()) {
+        
+        System.out.println("Starting PowerLaunchJava...")
+        System.out.println("PowerLaunchJava is OK! But now, read this:")
+        System.out.println("")
+        System.out.println("By using this plugin, you agree to the project license (Mozilla Public License v2.0) and accept that the plugin may make changes to your Minecraft server.
+- PowerLaunchJava System")
+
+    }
+}
+
+public class PowerLauncherJavaUtilities {
+
+    if(PowerLauncherJavaLauncher.not.exists) {
+        System.out.println("The class "PowerLaunchJavaLauncher" don't exists! Please restart this plugin!")
+    }
+
+    else(PowerLauncherJavaLauncher.exists) {
+        System.out.println("I Check all of files and... All files and folders are ok! I Will Start!")
+    }
+}
+
+public class LauncherFiles {
+    powerlaunchjava.file("launcher.yml", "PowerLaunchJava.java", "/resources/plugin.yml")
+    System.out.println("All of files are ok!")
+}
