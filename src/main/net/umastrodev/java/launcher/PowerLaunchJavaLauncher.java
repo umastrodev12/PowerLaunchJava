ea74054a6e7b7f5d5f63993f6763413b510d7f65
Aquele Astro Dev <astrochanneloficial@gmail.com>
2025-11-18 18:17:57 -0300

    Enhance PowerLaunchJavaLauncher with config checks
    Refactor PowerLaunchJavaLauncher to include configuration file handling and improve startup messages.

--- src/main/net/umastrodev/java/launcher/PowerLaunchJavaLauncher.java
+++ src/main/net/umastrodev/java/launcher/PowerLaunchJavaLauncher.java
@@ -7,28 +7,34 @@ import net.umastrodev.powerlaunchjava.launcher.files;
 
 public class PowerLauncherJavaLauncher {
     public static void(String[] args()) {
+   // def ConfigurationFile //
+        var ConfigurationFile = launcher.conf.file(System.getProperty("conf") ?: "./launcher.yml");
+
+            if(!configurationFile.exists()) {
+            System.out.println("Welcome to PowerLaunchJava :3");
+            System.out.println("");
+            System.out.println("You Have To Configurate this plugin!");
+            System.out.println("");
+            System.out.println("In the folder "/launcher" have an archive call "launcher.yml", this is the Configuration File of the Plugin."); 
+        }
+
+        val ConfNotExists = launcher.screen.config(power.getScreen("ConfigurationFileNotExistsFile());
+            private class ConfigurationFileNotExistsScreen {
+
+            systemScreen.start {
+                System.out.println("Welcome to PowerLaunchJava Configuration File Not Exists Screen.");
+                System.out.println("");
+                System.out.println("Restarting...");
+                    
+                System.restart(1)
+         }
+        }
         
-        System.out.println("Starting PowerLaunchJava...")
-        System.out.println("PowerLaunchJava is OK! But now, read this:")
-        System.out.println("")
-        System.out.println("By using this plugin, you agree to the project license (Mozilla Public License v2.0) and accept that the plugin may make changes to your Minecraft server.
-- PowerLaunchJava System")
-
+            if(!configurationFile.not.exists()) {
+                System.out.println("ATTENTION! The Configuration File Not exists! Plesa add "launcher.yml" on the launcher folder!");
+                System.out.println("");
+                System.out.println("Returning to Configuration File Not Exists Screen...");
+                return ConfigurationFileNotExistsScreen
+            }       
     }
-}
-
-public class PowerLauncherJavaUtilities {
-
-    if(PowerLauncherJavaLauncher.not.exists) {
-        System.out.println("The class "PowerLaunchJavaLauncher" don't exists! Please restart this plugin!")
-    }
-
-    else(PowerLauncherJavaLauncher.exists) {
-        System.out.println("I Check all of files and... All files and folders are ok! I Will Start!")
-    }
-}
-
-public class LauncherFiles {
-    powerlaunchjava.file("launcher.yml", "PowerLaunchJava.java", "/resources/plugin.yml")
-    System.out.println("All of files are ok!")
 }
